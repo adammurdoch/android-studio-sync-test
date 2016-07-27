aef80c3b95de10a5df6c74c68d7c8b949da86247
Adam Murdoch <adam@gradle.com>
2016-07-28 09:33:43 +1000

    Include all variants when inspecting the model.

--- src/main/java/net/rubygrapefruit/test/Main.java
+++ src/main/java/net/rubygrapefruit/test/Main.java
@@ -29,12 +29,12 @@ public class Main {
             modelBuilder.setStandardOutput(System.out);
             modelBuilder.setStandardError(System.err);
             modelBuilder.withArguments("-Dcom.android.build.gradle.overrideVersionCheck=true");
-            modelBuilder.setJvmArguments("-Xmx7g");
+            modelBuilder.setJvmArguments("-Xmx2g");
             Map<String, AndroidProject> models = modelBuilder.run();
 
             System.out.println("Received models: " + models.size());
 
-            dump(models);
+            inspectModel(models);
         } finally {
             connect.close();
         }
@@ -43,7 +43,7 @@ public class Main {
         System.out.println();
     }
 
-    private static void dump(Map<String, AndroidProject> models) {
+    private static void inspectModel(Map<String, AndroidProject> models) {
         Set<JavaLibrary> javaLibsByEquality = new HashSet<>();
         Map<File, JavaLibrary> javaLibsByFile = new HashMap<>();
         Map<JavaLibrary, JavaLibrary> javaLibsByIdentity = new IdentityHashMap<>();
@@ -56,22 +56,20 @@ public class Main {
             if (androidProject == null) {
                 continue;
             }
-            System.out.println(androidProject.getName());
             for (Variant variant : androidProject.getVariants()) {
-                if (variant.getName().equals("debug")) {
-                    Dependencies dependencies = variant.getMainArtifact().getDependencies();
-                    System.out.println("android libs: " + dependencies.getLibraries().size());
-                    for (AndroidLibrary androidLibrary : dependencies.getLibraries()) {
-                        libsByEquality.add(androidLibrary);
-                        libsByFile.put(androidLibrary.getJarFile(), androidLibrary);
-                        libsByIdentity.put(androidLibrary, androidLibrary);
-                    }
-                    System.out.println("java libs: " + dependencies.getJavaLibraries().size());
-                    for (JavaLibrary javaLibrary : dependencies.getJavaLibraries()) {
-                        javaLibsByEquality.add(javaLibrary);
-                        javaLibsByFile.putIfAbsent(javaLibrary.getJarFile(), javaLibrary);
-                        javaLibsByIdentity.putIfAbsent(javaLibrary, javaLibrary);
-                    }
+                System.out.println(androidProject.getName() + " " + variant.getName());
+                Dependencies dependencies = variant.getMainArtifact().getDependencies();
+                System.out.println("android libs: " + dependencies.getLibraries().size());
+                for (AndroidLibrary androidLibrary : dependencies.getLibraries()) {
+                    libsByEquality.add(androidLibrary);
+                    libsByFile.put(androidLibrary.getJarFile(), androidLibrary);
+                    libsByIdentity.put(androidLibrary, androidLibrary);
+                }
+                System.out.println("java libs: " + dependencies.getJavaLibraries().size());
+                for (JavaLibrary javaLibrary : dependencies.getJavaLibraries()) {
+                    javaLibsByEquality.add(javaLibrary);
+                    javaLibsByFile.putIfAbsent(javaLibrary.getJarFile(), javaLibrary);
+                    javaLibsByIdentity.putIfAbsent(javaLibrary, javaLibrary);
                 }
             }
         }
@@ -85,18 +83,17 @@ public class Main {
         System.out.println("---");
     }
 
-    static class GetModel implements BuildAction<Map<String, AndroidProject>> {
+    private static class GetModel implements BuildAction<Map<String, AndroidProject>> {
         @Override
         public Map<String, AndroidProject> execute(BuildController controller) {
             System.out.println("fetching");
             GradleBuild build = controller.getBuildModel();
-            System.out.println("projects: " + build.getProjects().size());
             Map<String, AndroidProject> result = new TreeMap<>();
             for (BasicGradleProject project : build.getProjects()) {
                 AndroidProject androidProject = controller.findModel(project, AndroidProject.class);
                 result.put(project.getPath(), androidProject);
             }
-            dump(result);
+            inspectModel(result);
             return result;
         }
     }
